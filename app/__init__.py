bb936402dfaf2e0f5d3b6196808f3cc16c3f953d
Toui Mori <92294389+ghtoui@users.noreply.github.com>
2023-02-28 12:58:55 +0900

    html作成

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 import cv2
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+# configfileの読み込み
+app.config.from_object('app.config')
+
+# これは最後に書く
 from app import view
--- app/view.py
+++ app/view.py
@@ -1,5 +1,6 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return 'Hellow World'
+    return render_template('/index.html')
